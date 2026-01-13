c78da50188021924eea036d77b4bc6afddbad41c
chris k <yovirinc@gmail.com>
2026-01-13 01:36:26 +0100

    .gitignore

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$hello = "hello index";
+function hello_man() {
+  echo "hello man";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $hello ?></title>
+</head>
+
+<body>
+  body;
+</body>
+
+</html>
