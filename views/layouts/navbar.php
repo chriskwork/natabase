484c314e971e8528abf4af3f4d34f4ba735ba95a
chris k <yovirinc@gmail.com>
2026-01-25 20:07:49 +0100

    hamburger menu error fixed

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -5,13 +5,6 @@
       <img src="public/assets/imgs/logo.png" alt="LOGO" class="w-auto h-10" />
     </a>
 
-    <!-- mobile menu -->
-    <button id="menu-toggle" class="lg:hidden flex flex-col gap-1.5 p-2">
-      <span class="block bg-gray-700 w-6 h-0.5"></span>
-      <span class="block bg-gray-700 w-6 h-0.5"></span>
-      <span class="block bg-gray-700 w-6 h-0.5"></span>
-    </button>
-
     <!-- nav(desktop) -->
     <ul class="hidden lg:flex items-center gap-12">
       <li class="text-gray-700 hover:text-brand-turquoise transition-colors duration-200 cursor-pointer">
@@ -28,14 +21,25 @@
       </li>
     </ul>
 
+    <!-- hamburger menu -->
+    <button id="menu-toggle" class="lg:hidden flex flex-col gap-1.5 p-2 cursor-pointer">
+      <span class="block bg-gray-700 w-6 h-0.5"></span>
+      <span class="block bg-gray-700 w-6 h-0.5"></span>
+      <span class="block bg-gray-700 w-6 h-0.5"></span>
+    </button>
+
     <!-- action button -->
     <div class="hidden lg:flex items-center gap-x-10">
       <a href="#">Iniciar Sesión</a>
-      <a href="#" class="bg-brand-turquoise hover:bg-[#0088b3] shadow-sm hover:shadow-md px-6 py-2 rounded-lg font-semibold text-white transition-colors duration-200">
+      <a href="#" class="bg-brand-point hover:bg-[#e64c22] shadow-sm hover:shadow-md px-6 py-2 rounded-lg font-semibold text-white transition-colors duration-200">
         Hazte socio
       </a>
     </div>
   </div>
+
+
+
+
 
   <!-- 모바일 메뉴 (토글) -->
   <div id="mobile-menu" class="hidden lg:hidden px-4 pb-4">
@@ -55,7 +59,7 @@
     </ul>
     <div class="flex flex-col gap-3">
       <a href="#" class="text-center">Iniciar Sesión</a>
-      <a href="#" class="bg-brand-turquoise hover:bg-[#0088b3] shadow-sm hover:shadow-md px-6 py-2 rounded-lg font-semibold text-white text-center transition-colors duration-200">
+      <a href="#" class="bg-brand-point hover:bg-[#e64c22] shadow-sm hover:shadow-md px-6 py-2 rounded-lg font-semibold text-white text-center transition-colors duration-200">
         Hazte socio
       </a>
     </div>
