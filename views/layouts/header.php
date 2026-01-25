b8b66bc9a85b50db5cf5fb791eaeb7ef976d115b
chris k <yovirinc@gmail.com>
2026-01-25 20:19:48 +0100

    Added material symbol link

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,7 +1,6 @@
 <?php
 ?>
 
-<!-- ctrl alt T // tailwind docs in vscode -->
 <!DOCTYPE html>
 <html lang="es">
 
@@ -9,6 +8,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NataBase</title>
+  <!-- material icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <!-- css -->
   <link rel="stylesheet" href="public/assets/css/style.css?v=<?= time() ?>">
 </head>
 
