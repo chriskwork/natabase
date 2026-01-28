46788dbb400dadf0d1b4a2275e49bf74769df1de
yohan <yovirinc@gmail.com>
2026-01-28 19:52:21 +0100

    changed dir url

--- index.php
+++ index.php
@@ -4,33 +4,33 @@
 ?>
 
 <!-- header section -->
-<?php require_once "./views/layouts/header.php"; ?>
+<?php require_once __DIR__ . "/views/layouts/header.php"; ?>
 
 <!-- landing page -->
-<?php require_once "./views/layouts/index_sections/hero.php"; ?>
+<?php require_once __DIR__ . "/views/layouts/index_sections/hero.php"; ?>
 
 <!-- main contents -->
 <main class="mx-auto mt-14">
 
   <!-- sobre nosotros (el club) -->
-  <?php require_once "./views/layouts/index_sections/sobre_nos.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/sobre_nos.php"; ?>
 
   <!-- programs section -->
-  <?php require_once "./views/layouts/index_sections/program.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/program.php"; ?>
 
   <!-- cifras section -->
-  <?php require_once "./views/layouts/index_sections/cifras.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/cifras.php"; ?>
 
   <!-- eventos section -->
-  <?php require_once "./views/layouts/index_sections/events.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/events.php"; ?>
 
   <!-- precios section -->
-  <?php require_once "./views/layouts/index_sections/precios.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/precios.php"; ?>
 
   <!-- contacto section -->
-  <?php require_once "./views/layouts/index_sections/contact.php"; ?>
+  <?php require_once __DIR__ . "/views/layouts/index_sections/contact.php"; ?>
 
 </main>
 
 <!-- footer section -->
-<?php require_once "./views/layouts/footer.php"; ?>
+<?php require_once __DIR__ . "/views/layouts/footer.php"; ?>
